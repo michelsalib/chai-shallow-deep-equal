7f3087d951cae3674579ca65d85b653c9dddafd4
Michel Salib <michel.salib@thomsonreuters.com>
2016-09-28 16:09:23 +0200

    Added typings

---
+++ chai-shallow-deep-equal.d.ts
@@ -0,0 +1,19 @@
+declare module Chai {
+
+    // For Assert API
+    interface Assert {
+        shallowDeepEqual(actual: any, expected: any, message?: string);
+    }
+
+    // For Chai as promised
+    interface PromisedAssert {
+        shallowDeepEqual(actual: any, expected: any, message?: string): any;
+    }
+
+}
+
+declare module "chai-shallow-deep-equal" {
+    function chaiShallowDeepEqual(chai: any, utils: any): void;
+    namespace chaiShallowDeepEqual{}
+    export = chaiShallowDeepEqual;
+}
